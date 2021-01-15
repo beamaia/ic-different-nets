ce6a8124c11017dcf67edcd100cdd0c252bf5318
bea <beamaia.work@gmail.com>
2021-01-15 17:11:40 -0300

    Saved models as of 15-01-2021

---
+++ data.py
@@ -0,0 +1,132 @@
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+
+from glob import glob
+import fnmatch
+import cv2
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.image import PatchExtractor
+import numpy as np
+
+def image_paths(First_Set=True, Both_Set=False, Hinton=False, home=False):
+    PATH_normal100x = 'First Set/100x Normal Oral Cavity Histopathological Images/*'
+    PATH_carcinoma100x = 'First Set/100x OSCC Histopathological Images/*'
+    PATH_normal400x = 'Second Set/400x Normal Oral Cavity Histopathological Images/*'
+    PATH_carcinoma400x = 'Second Set/400x OSCC Histopathological Images/*'
+    
+    if home:
+        PATH_ini = ".././data/oralCancer-Borooah/"
+    elif Hinton:
+        PATH_ini = "./home/Desktop/Humberto/data/"
+    else:
+        PATH_ini = "./data/oralCancer-Borooah/"
+
+    if not Both_Set:
+        if First_Set:
+            PATH_normal = PATH_ini + PATH_normal100x
+            PATH_carcinoma = PATH_ini + PATH_carcinoma100x
+        else:
+            PATH_normal = PATH_ini + PATH_normal400x
+            PATH_carcinoma = PATH_ini + PATH_carcinoma400x
+        
+        images_normal = glob(PATH_normal)
+        images_carcinoma = glob(PATH_carcinoma)
+
+    else:
+        PATH_normal100 = PATH_ini + PATH_normal100x
+        PATH_carcinoma100 = PATH_ini + PATH_carcinoma100x
+        PATH_normal400 = PATH_ini + PATH_normal400x
+        PATH_carcinoma400 = PATH_ini + PATH_carcinoma400x
+        images_normal100 = glob(PATH_normal100)
+        images_carcinoma100 = glob(PATH_carcinoma100)
+        images_normal400 = glob(PATH_normal400)
+        images_carcinoma400 = glob(PATH_carcinoma400)
+
+        images_normal = images_normal100 + images_normal400
+        images_carcinoma = images_carcinoma100 + images_carcinoma400
+
+    print(f"Found {len(images_normal)} images of class: normal and {len(images_carcinoma)} images of class: carcinoma", end="\n\n")
+    return images_normal, images_carcinoma
+
+def process_images(images):
+    height, width = 250, 250
+    
+    x = []
+        
+    for _, img in enumerate(images):
+        full_size_image = cv2.imread(img)
+        image = (cv2.resize(full_size_image, (width, height), interpolation = cv2.INTER_CUBIC))
+        
+        x.append(image)
+
+    return np.array(x)
+
+def create_images_labels(x_normal, x_carcinoma):
+    # Creating patches
+    pe = PatchExtractor(patch_size = (30, 30), max_patches = 30)
+    patches_normal = pe.transform(x_normal)
+    patches_carcinoma = pe.transform(x_carcinoma)
+    images = np.concatenate((patches_normal, patches_carcinoma), axis = 0)
+
+    labels = []
+
+    labels_nr = [0 for i,_ in enumerate(patches_normal)]
+    labels_ca = [1 for i,_ in enumerate(patches_carcinoma)]
+    labels = labels_nr + labels_ca
+
+    labels = np.array(labels)
+
+    images = torch.from_numpy(images)
+    labels = torch.Tensor(labels) 
+
+    images = images.type(torch.FloatTensor) 
+    labels = labels.type(torch.LongTensor) 
+
+    return images, labels
+
+def create_train_test(images, labels, test_size):
+    x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=test_size, random_state=42)
+    x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size =0.5, random_state=42)
+
+    return (x_train, y_train), (x_val, y_val), (x_test, y_test)
+
+# DataLoader
+
+class DatasetOral(torch.utils.data.Dataset):
+    def __init__(self, images, labels, transformation):
+        self.labels = labels
+        self.images = images
+        self.transformation = transformation
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, index):
+        return self.images[index], self.labels[index]
+
+def create_dataloaders(x_train, y_train, x_test, y_test, x_val, y_val, batch_size=64, shuffle=True, num_workers=2):
+    params = {'batch_size': batch_size,
+              'shuffle': shuffle,
+              'num_workers': num_workers}
+
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    x_train = x_train.reshape(-1, 3, 30, 30)
+    x_val = x_val.reshape(-1, 3, 30, 30)
+    x_test = x_test.reshape(-1, 3, 30, 30)
+
+    train_set = DatasetOral(x_train, y_train, transform)
+    train_loader = DataLoader(train_set, **params)
+
+    val_set = DatasetOral(x_val, y_val, transform)
+    val_loader = DataLoader(val_set, **params)
+
+    test_set = DatasetOral(x_test, y_test, transform)
+    test_loader = DataLoader(test_set, **params)
+
+    return train_loader, val_loader, test_loader
+
---
+++ main.py
@@ -0,0 +1,108 @@
+import torch
+import torchvision
+from torchvision import models
+import torch.nn as nn
+
+import numpy as np
+from os import system
+
+import data as dt
+import train as tr
+import utils
+
+def get_info():
+    sets = int(input("(1) First set, (2) Second Set, (3) Both \n"))
+    epochs = int(input("Number of epochs:\n"))
+    lr = float(input("Learning rate:\n"))
+
+    first_set = False
+    both_sets = False
+
+    if sets == 1:
+        first_set = True
+    elif sets==3:
+        both_sets = True
+
+    dp = float(input("Dropout rate:\n"))
+    model_name = input("Model name:\nShould be 'resnet50', 'mobilenetv2' or 'inceptionv3'\n")
+    date = input("Date:\n")
+    version = int(input("Version:\n"))
+    hinton = bool(input("Hinton:\n"))
+    bea = bool(input("Bea:\n"))
+
+    system("clear")
+    return first_set, both_sets, epochs, lr, sets, dp, model_name, date, version, hinton, bea
+
+def main():
+
+    first_set, both_sets, epochs, lr, sets, dp, model_name, date, version, hinton, bea = get_info()
+    # images
+    print("Processing images...")
+    images_normal, images_carcinoma = dt.image_paths(First_Set=first_set, Both_Set= both_sets, Hinton=hinton, home=bea)
+    x_normal = dt.process_images(images_normal)
+    x_carcinoma = dt.process_images(images_carcinoma)
+
+    images, labels = dt.create_images_labels(x_normal, x_carcinoma)
+
+    print("Creating train, validation and test images...")
+    # split
+    (x_train, y_train), (x_val, y_val), (x_test, y_test) = dt.create_train_test(images, labels, 0.3)
+
+    # dataloaders
+    train_loader, val_loader, test_loader = dt.create_dataloaders(x_train, y_train, x_test, y_test, x_val, y_val)
+
+    print("Configuring model...",end="\n\n")
+    params = {
+    'num_epochs': epochs,
+    'lr': lr
+    }
+
+    # info 
+    # change here
+    # model_name = "resnet50"
+    # date = "15-01-2020"
+    # version = 3
+    
+    if model_name == "resnet50":
+        model = models.resnet50(pretrained=True)
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Sequential(
+            nn.Dropout(dp), #change here
+            nn.Linear(num_ftrs, 10)
+        )
+    elif model_name == "mobilenetv2":
+        model = models.mobilenet_v2(pretrained=True)
+    elif model_name == "inceptionv3":
+        model = models.inception_v3(pretrained=True)
+        print(model)
+
+    # train
+    train_accuracies, train_losses, val_accuracies, val_losses, y_predict = tr.train(model, train_loader, val_loader, **params)
+
+    print("Saving training data...", end="\n\n")
+    # transforming into np.array
+    array_train_accuracies = np.asarray(train_accuracies)
+    array_train_losses = np.asarray(train_losses)
+    array_val_accuracies = np.asarray(val_accuracies)
+    array_val_losses = np.asarray(val_losses)
+
+    # saving data
+    PATH = './saved_models/' + str(model_name) + "_" + str(date) + "_" + str(version)
+    torch.save(model.state_dict(), PATH)
+
+    utils.save_txt_accuracy_loss(array_train_accuracies, array_train_losses, date, model_name, version, training=True)
+    utils.plot_accuracy_loss(epochs, model_name, losses=array_train_losses, accuracies=array_train_accuracies, date=date, version=version, training=True)
+
+    utils.save_txt_accuracy_loss(array_val_accuracies, array_val_losses, date, model_name, version, training=False)
+    utils.plot_accuracy_loss(epochs, model_name, losses=array_val_losses, accuracies=array_val_accuracies, date=date, version=version, training=False)
+
+    # test
+    test_accuracy, y_predict = tr.test(model, test_loader)
+
+    print("Saving test data...")
+
+    utils.save_test_accuracy(test_accuracy, model_name, date, version, new=False, sets=sets) # change here
+    utils.save_dataframes(y_test, y_predict, model_name, date, version)
+
+if __name__ == "__main__":
+    main()
